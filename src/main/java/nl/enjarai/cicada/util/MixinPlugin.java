efd67ca9b5db9bbdfe9984c9c2750b50cb34cdc3
enjarai <enjarai123@gmail.com>
2023-09-16 17:02:05 +0200

    1.20.2 oh boy

---
+++ src/main/java/nl/enjarai/cicada/util/MixinPlugin.java
@@ -0,0 +1,54 @@
+package nl.enjarai.cicada.util;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.VersionParsingException;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        try {
+            //noinspection OptionalGetWithoutIsPresent
+            return FabricLoader.getInstance().getModContainer("minecraft").get()
+                    .getMetadata().getVersion().compareTo(Version.parse("1.20.1")) <= 0;
+        } catch (RuntimeException | VersionParsingException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
