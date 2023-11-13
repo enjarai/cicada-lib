50e6ae97e3d3932773c9bdd868b08d1216e57937
enjarai <enjarai123@gmail.com>
2023-11-13 13:36:17 +0100

    Figuring this out

--- src/main/java/nl/enjarai/cicada/mixin/PlayerListEntryMixin.java
+++ src/main/java/nl/enjarai/cicada/mixin/PlayerListEntryMixin.java
@@ -1,8 +1,10 @@
-package nl.enjarai.cicada.mixin.post_20_2;
+package nl.enjarai.cicada.mixin;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.network.PlayerListEntry;
+/*? >=1.20.2 {?*//*
 import net.minecraft.client.util.SkinTextures;
+/*?}?*/
 import net.minecraft.util.Identifier;
 import nl.enjarai.cicada.util.CapeHandler;
 import org.spongepowered.asm.mixin.Final;
@@ -10,16 +12,35 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.Supplier;
 
 @Mixin(PlayerListEntry.class)
-public class PlayerListEntryMixin {
+public abstract class PlayerListEntryMixin {
     @Shadow
     @Final
     private GameProfile profile;
 
+    /*? <=1.20.1 {?*/
+    @Shadow private boolean texturesLoaded;
+
+    @Inject(method = "loadTextures", at = @At("HEAD"))
+    private void cicada$loadTextures(CallbackInfo ci) {
+        if (!texturesLoaded) {
+            CapeHandler.onLoadTexture(profile);
+        }
+    }
+
+    @Inject(method = "getCapeTexture", at = @At("TAIL"), cancellable = true)
+    private void cicada$getCapeTexture(CallbackInfoReturnable<Identifier> cir) {
+        CapeHandler handler = CapeHandler.fromProfile(profile);
+        if (handler.hasCape() || handler.hasElytra()) {
+            cir.setReturnValue(handler.getCapeTexture());
+        }
+    }
+    /*?} else {?*//*
     @Inject(method = "texturesSupplier", at = @At("HEAD"))
     private static void loadTextures(GameProfile profile, CallbackInfoReturnable<Supplier<SkinTextures>> cir) {
         CapeHandler.onLoadTexture(profile);
@@ -39,4 +60,5 @@ public class PlayerListEntryMixin {
             cir.setReturnValue(newTextures);
         }
     }
+    /*?}?*/
 }
