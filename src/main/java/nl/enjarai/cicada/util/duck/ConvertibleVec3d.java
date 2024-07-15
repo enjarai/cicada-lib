266f12347cb825f07d4f2136b5b49008e271ea81
enjarai <enjarai123@gmail.com>
2024-07-15 23:16:56 +0200

    Ok does that work?

--- src/main/java/nl/enjarai/cicada/util/duck/ConvertibleVec3d.java
+++ src/main/java/nl/enjarai/cicada/util/duck/ConvertibleVec3d.java
@@ -1,11 +1,16 @@
 package nl.enjarai.cicada.util.duck;
 
 import net.minecraft.util.math.Vec3d;
+import org.apache.commons.lang3.NotImplementedException;
 import org.joml.Vector3d;
 import org.joml.Vector3dc;
 
 public interface ConvertibleVec3d {
-    Vector3d toVector3d();
+    default Vector3d toVector3d() {
+        throw new NotImplementedException("Mixin in cicada broke!! WHaT!?");
+    }
 
-    Vec3d fromVector3d(Vector3dc vector);
+    default Vec3d fromVector3d(Vector3dc vector) {
+        throw new NotImplementedException("Mixin in cicada broke!! WHaT!?");
+    }
 }
--- src/main/java/nl/enjarai/cicada/util/duck/ConvertibleVec3i.java
+++ src/main/java/nl/enjarai/cicada/util/duck/ConvertibleVec3i.java
@@ -1,11 +1,16 @@
 package nl.enjarai.cicada.util.duck;
 
 import net.minecraft.util.math.Vec3i;
+import org.apache.commons.lang3.NotImplementedException;
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
 
 public interface ConvertibleVec3i {
-    Vector3i toVector3i();
+    default Vector3i toVector3i() {
+        throw new NotImplementedException("Mixin in cicada broke!! WHaT!?");
+    }
 
-    Vec3i fromVector3i(Vector3ic vector);
+    default Vec3i fromVector3i(Vector3ic vector) {
+        throw new NotImplementedException("Mixin in cicada broke!! WHaT!?");
+    }
 }
