394f0214fe39be0719daf1220a1554c8fc1afa09
enjarai <enjarai@trans.fish>
2025-10-05 11:57:21 +0200

    im smart

--- src/main/java/nl/enjarai/cicada/Cicada.java
+++ src/main/java/nl/enjarai/cicada/Cicada.java
@@ -9,13 +9,12 @@ import nl.enjarai.cicada.api.util.CicadaEntrypoint;
 import nl.enjarai.cicada.api.util.JsonSource;
 import nl.enjarai.cicada.api.util.ProperLogger;
 import nl.enjarai.cicada.api.util.YamlSource;
-import nl.enjarai.cicada.util.CapeHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
 
-public final class Cicada implements ModInitializer, ClientModInitializer, CicadaEntrypoint {
+public final class Cicada implements ModInitializer, CicadaEntrypoint {
 	public static final String MOD_ID = "cicada";
 	public static final Logger LOGGER = ProperLogger.getLogger(MOD_ID);
 	public static final Logger QUOTE_OF_THE_DAY_LOGGER = LoggerFactory.getLogger("Library Quote of the Day");
@@ -31,11 +30,6 @@ public final class Cicada implements ModInitializer, ClientModInitializer, Cicad
 			CONVERSATION_MANAGER.run();
 			ConversationManager.getThreadPool().shutdown();
 		}, ConversationManager.getThreadPool());
-	}
-
-	@Override
-	public void onInitializeClient() {
-		ClientLifecycleEvents.CLIENT_STOPPING.register(client -> CapeHandler.shutdown());
 	}
 
 	@Override
